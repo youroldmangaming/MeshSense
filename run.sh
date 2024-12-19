f45269ee1b9ee4a5418260b55db9b0641a41a222
Mike Wilson <youroldmangaming@gmail.com>
2024-12-19 22:49:29 +1300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,16 @@
-docker run -d --name meshsense -p 5920:5920 -p 5921:5921 --privileged meshsense:latest -e ACCESS_KEY="noSecrets" \
+docker run -d \
+  --name meshsense \
+  -p 5921:5921 \
+  -p 5920:5920 \
+  -e PORT=5921 \
+  -e DEV_UI_URL="http://localhost:5921" \
+  -e ACCESS_KEY="noSecrets" \
+  --tty \
+  --interactive \
+  --privileged \
+  y0mg/meshsense
+
+
+
+
+
