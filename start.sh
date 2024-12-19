6428a5069be49245be6d00328118457648b7e7b1
Mike Wilson <youroldmangaming@gmail.com>
2024-12-19 22:03:54 +1300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,14 +8,13 @@ export DEV_UI_URL=http://localhost:5921
 # Start UI service
 echo "Starting UI service..."
 cd /app/MeshSense/ui
-npm run dev &
+PORT=5921 npm run dev &
 
 # Start API service
 echo "Starting API service..."
 cd /app/MeshSense/api
-npm run dev &
+ACCESS_KEY=${ACCESS_KEY} PORT=5920 npm run dev & &
 
 # Keep container running
 wait
-
 
